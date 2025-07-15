7687caf94efc41ae430ce265afd18bf3e439a799
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-07-15 14:50:23 +0200

    Injecting mailer on quotation creation for sending email

--- src/app/quotation/actions/quotation-actions.ts
+++ src/app/quotation/actions/quotation-actions.ts
@@ -6,10 +6,16 @@ import { redirect } from "next/navigation";
 import { QuotationAppService } from "../application/services/quotation-service";
 import { PrismaQuotationRepository } from "../infrastructure/repositories/quotation-repository";
 import { PrismaClientRepository } from "../infrastructure/repositories/client-repository";
+import { getCurrentUser } from "../../user-auth/actions/login-actions";
+import { NodemailerMailer } from "@/src/app/mail/infrastructure/nodemailer-mailer";
+import { MailService } from "@/src/app/mail/application/services/MailService";
+import { CurrentUserServiceImpl } from "../../user-auth/infrastructure/services/current-user-service";
 
 const quotationService = new QuotationAppService(
   new PrismaQuotationRepository(),
-  new PrismaClientRepository()
+  new PrismaClientRepository(),
+  new MailService(new NodemailerMailer()),
+  new CurrentUserServiceImpl()
 );
 
 export async function createQuotation(formData: FormData) {
@@ -30,14 +36,19 @@ export async function createQuotation(formData: FormData) {
     };
   });
 
+  const userResponse = await getCurrentUser();
+  const user = userResponse.user;
+  const userId = user?.id;
+
   await quotationService.createQuotation({
     id: crypto.randomUUID(),
     version: 1,
     clientId: clientId!,
     lines,
     taxRate,
+    userId,
   });
 
   revalidatePath("/quotation");
-  redirect("/quotation");
+  redirect("/quotation/list");
 }
