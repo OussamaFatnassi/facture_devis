3cba5d1036c893a92299aa1d2349a012fbce2519
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-07-17 12:22:40 +0200

    Search by Product Controller

---
+++ src/app/search/infrastructure/controllers/by-product-controller.ts
@@ -0,0 +1,11 @@
+import { searchByProduct } from "../../application/SearchByProduct";
+import { PrismaQuotationRepository } from "@/src/app/quotation/infrastructure/repositories/quotation-repository";
+import { PrismaInvoiceRepository } from "@/src/app/invoice/infrastructure/repositories/invoice-repository";
+
+export async function byProductController(productName: string) {
+  return searchByProduct(
+    productName,
+    new PrismaQuotationRepository(),
+    new PrismaInvoiceRepository()
+  );
+}
